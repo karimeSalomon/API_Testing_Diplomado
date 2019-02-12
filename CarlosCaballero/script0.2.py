8a10db99fecebe7bebfb5b69115d48fe801cfff7
Carlos E. Caballero B <cijkb.j@gmail.com>
2019-02-12 17:29:25 -0400

    completed the first practice

--- CarlosCaballero/script0.2.py
+++ CarlosCaballero/script0.2.py
@@ -1,22 +1,22 @@
-def add(a,b):
-    return a+b
-
-def minus(a,b):
-    return a-b
-
-def multiply(a,b):
-    return a*b
-
-def divide(a,b):
-    return a/b
+# practice #1
 
 def perform_operation(operator,a,b):
     dict={
-        '+':add(a,b),
-        '-':minus(a,b),
-        '*':multiply(a,b)
+        '+':a+b,
+        '-':a-b,
+        '*':a*b,
+        '/':a/b,
+        '**':a**b
     }
+
     return dict.get(operator)
 
-print(perform_operation('*',5,6))
+print('perform_operation(\'+\',1,2)    => ',perform_operation('+',1,2))
+print('perform_operation(\'-\',5,9)    => ',perform_operation('-',5,9))
+print('perform_operation(\'*\',-1,2)   => ',perform_operation('*',-1,2))
+print('perform_operation(\'/\',0,22)   => ',perform_operation('/',0,22))
+print('perform_operation(\'%\',111,4)  => ',perform_operation('%',111,4))
+print('perform_operation(\'**+\',2,32) => ',perform_operation('**',2,32))
+print('perform_operation(\'//\',10,3)  => ',perform_operation('//',10,3))
+print('perform_operation(\'\',10,10)   => ',perform_operation('',10,10))
 
