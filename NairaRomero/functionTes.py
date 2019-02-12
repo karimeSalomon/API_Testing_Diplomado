2b2a8ebda8b659bfe93b28a868af28e87e60c962
Naira Romero <naira88re@gmail.com>
2019-02-11 20:55:28 -0400

    operators and functions practice

---
+++ NairaRomero/functionTes.py
@@ -0,0 +1,5 @@
+def my_first_function():
+    "This  is the line reserved for documentation docstring"
+    print("I'm a QA, I'm Ok")
+    print("testing my frist function in Python")
+my_first_function()
