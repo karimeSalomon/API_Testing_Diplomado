06fe5dfe84460038ac634461d4291448a425caa3
Ubaldo Villaseca <ubaldo.villaseca@vipre.com>
2019-02-12 18:56:59 -0400

    N/A. Implemented practice arithmetic operators.

---
+++ UbaldoVillaseca/arithmetic_operators.py
@@ -0,0 +1,13 @@
+a_ver_long_long_varible_name_like_this = "ok"
+print(a_ver_long_long_varible_name_like_this)
+a_variable_with_4_numbers_1234 = "1234"
+print(a_variable_with_4_numbers_1234)
+a = 31
+b = 13
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a**b)
+print(a // b)
