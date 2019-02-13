75014cf35df5c83b4d418d6d2967381bc7b95935
ICSC <ICSC@fundacion-jala.local>
2019-02-12 21:59:12 -0400

    Modificando la practica 1 con operadores y resultados impresos + la tarea de ParImpar

--- MarielaSejas/practice1.py
+++ MarielaSejas/practice1.py
@@ -3,21 +3,34 @@ def print_TestMariela(a, b, c):
 
     if (a == "*"):
         resultado = b*c
+        print("El resultado de la multiplicion entre  " +str(b) + " = " +str(c) + " es:")
 
 
     if (a == "/"):
         resultado = b / c
-
+        print("El resultado de la division entre  " + str(b) + " = " + str(c) + " es:")
 
     if (a == "-"):
         resultado = b - c
-
+        print("El resultado de la resta entre  " + str(b) + " = " + str(c) + " es:")
 
     if (a == "+"):
         resultado = b + c
+        print("El resultado de la suma entre  " + str(b) + " = " + str(c) + " es:")
 
+    if (a == "%"):
+        resultado = b % c
+        print("El resultado del modulo entre  " + str(b) + " = " + str(c) + " es:")
+
+    if (a == "**"):
+        resultado = b ** c
+        print("El resultado del exponente entre  " + str(b) + " = " + str(c) + " es:")
+
+    if (a == "//"):
+        resultado = b // c
+        print("El resultado del Floor Division entre  " + str(b) + " = " + str(c) + " es:")
 
     print(resultado)
 
 
-print_TestMariela("*", 5, 5)
+print_TestMariela("//", 5, 5)
