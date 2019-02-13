67e5af5227f47d97ec1f48d73adb5c35cf1cf377
ICSC <ICSC@fundacion-jala.local>
2019-02-12 21:44:29 -0400

    adding changes to sopport more operators and to display the numbers and operator applied

--- RolandoMamani/calculator.py
+++ RolandoMamani/calculator.py
@@ -1,17 +1,27 @@
 def my_calculator(operator, number1, number2):
     "This function is a calculator for main operator, +, -, * and /"
     if (operator == "*"):
-        print(number1 * number2)
+        print("The multimplication of " + str (number1) + "*" + str(number2) + " is " + str(number1 * number2))
     else:
         if(operator == "/"):
-            print(number2/number2)
+            print("The division of " + str(number1) + "/" + str(number2) + " is " + str(number1 / number2))
         else:
             if(operator == "-"):
-                print(number1-number2)
+                print("The subtraction of " + str(number1) + "-" + str(number2) + " is " + str(number1-number2))
             else:
                 if(operator == "+"):
-                    print(number1+number2)
+                    print("The addition of " + str(number1) + "+" + str(number2) + " is " + str(number1 + number2))
                 else:
-                    print("Sorry, operator not supported by this basic calculator...!!")
+                    if(operator == "%"):
+                        print("The module of " + str(number1) + "%" + str(number2) + " is " + str(number1 % number2))
+                    else:
+                        if(operator == "//"):
+                            print("The floor division of " + str(number1) + "/" + str(number2) + " is " + str(number1 // number2))
+                        else:
+                            if (operator == "**"):
+                                print("The exponent of " + str(number1) + " to " + str(number2) + " is " + str(number1 ** number2))
+                            else:
+                                print("Sorry, operator not supported by this basic calculator...!!")
 
-my_calculator("//",5,5)
+
+my_calculator("//",5,3)
