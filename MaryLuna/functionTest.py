a6a0ef1b3928d8478980c5c8023154a789922dc0
Mary Luna <jimenaluna84@gmail.com>
2019-02-12 20:34:30 -0400

    Fix operators

--- MaryLuna/functionTest.py
+++ MaryLuna/functionTest.py
@@ -9,19 +9,25 @@ print_lyrics();
 
 def perform_operation():
     "this is the lie reserved fro documentation"
-    operator = ["+", "-", "*"]
+    operator = ["+", "-", "*", "/"]
     for op in operator:
         a = 10
         b = 5
         print(op + " of %s and %s" % (a, b))
         if (op == "+"):
             res = a + b
-            if (op == "-"):
-                res = a - b
-            else:
-                res = a * b
+        if (op == "-"):
+            res = a - b
+        if (op == "*"):
+            res = a * b
+        if (op == "/"):
+            res = a / b
 
         print (res)
 
 
-perform_operation()
+perform_operation();
+
+
+
+
