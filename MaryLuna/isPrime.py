2dcf985a64aa4650d127a21de12e5c235e5c6d9d
Mary Luna <jimenaluna84@gmail.com>
2019-02-12 20:23:42 -0400

    isPrime

---
+++ MaryLuna/isPrime.py
@@ -0,0 +1,19 @@
+def isPrime(numMin, numMax):
+
+
+    limit = numMax +1
+    while(numMax < limit):
+            if numMin < 1:
+                # print (numMin + "no es primo")
+                print ("%s no es primo" % numMin)
+            elif numMin == 2:
+                print (str(numMin) + "es primo")
+            else:
+                    if numMin % 2 == 0:
+                        print ("%s no es primo" % numMin)
+                    else:
+                        print (str(numMin) + "es primo")
+    numMin+=1
+
+
+isPrime(1, 10)
