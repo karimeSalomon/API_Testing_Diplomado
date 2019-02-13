04568acfe1f478c76ce310123ae31dac86b32565
Horacio Crespo <horacio.crpo@gmail.com>
2019-02-12 21:22:07 -0400

    adding first practice

---
+++ HoracioCrespo/variables.py
@@ -0,0 +1,11 @@
+alphanumeric = "123456asdfgASDF*-."
+number1 = 10
+number2 = 5.5
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+print(number1**number2)
+print(number1 // number2)
