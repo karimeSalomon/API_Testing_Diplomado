eba1f79f428be8e7de6489ba33cd7c7793fd75e2
henry-benito-h <henry.benito.h@gmail.com>
2019-02-14 20:06:28 -0400

    Finishing task for dictionary

--- HenryBenito/third_class_dict.py
+++ HenryBenito/third_class_dict.py
@@ -2,9 +2,8 @@ song = "ThiS is String with Upper and lower case Letters"
 
 
 def count(input_string):
-    chars_list = list(input_string.lower()).sort()
     counters = {}
-    for char in chars_list:
+    for char in sorted(list(input_string.replace(' ', '').lower())):
         counters.setdefault(char, 0)
         counters[char] = counters.get(char) + 1
     return counters
