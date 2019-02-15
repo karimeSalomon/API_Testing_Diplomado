e5f8603fc461bbd325606a318e240995ddeb847e
HrodLandM <mclrolando@gmail.com>
2019-02-15 00:39:15 -0400

    added implemtation of getting the URL from a string

--- RolandoMamani/substringURL.py
+++ RolandoMamani/substringURL.py
@@ -1,15 +1,15 @@
 def substringURL(sentense):
     index_start = sentense.find("http://")
-    print(index_start)
+
     if(index_start == -1):
         print("The sentence does not contain any http:// URL")
     else:
-        for x in sentense:
-            print(x)
-            if(x==" "):
-                index_end = sentense.find(" ")
-                print("Encontre el espacio")
+        index_end = (sentense[index_start:]).find(" ")
+        if(index_end == -1):
+            url = (sentense[index_start:])
+        else:
+            url = (sentense[index_start:])[:((sentense[index_start:]).find(" "))]
 
+        print("The URL into the string is:   " + url)
 
-
-substringURL("Hola mundo http://jj jj")
+substringURL(input("Enter the string to verify if it contains a url\n"))
