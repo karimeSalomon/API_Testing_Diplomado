9f7549e5681ff6cd9e8be180b84044f74dd3842f
HrodLandM <mclrolando@gmail.com>
2019-02-16 15:41:18 -0400

    Added the refactory in the names of some files

--- RolandoMamani/Practice1_LongVariableAndMathOperators.py
+++ RolandoMamani/Practice1_LongVariableAndMathOperators.py
@@ -1,37 +1,4 @@
-"""
-Practice 3
-"""
 
-value_1 = 5
-value_2 = 9
-if (value_1 is value_2):
-    print("Line 1 - a and b have same identity")
-else:
-    print("Line 1 - a and b do not have same identity")
-
-if(id(value_1)==id(value_2)):
-    print("Line 1 - a and b have same identity")
-else:
-    print("Line 1 - a and b do not have same identity")
-print(id(value_1))
-print(id(value_2))
-
-
-"""
-Practice 2
-"""
-a = "a"
-b = "b"
-list = ["z", "g", "aa", "c", "k"]; listb = ["q", "a", "l", "r", "x"]
-
-if(a in listb):
-    print("a exist in the list")
-else:
-    print("a is not in the list")
-if(b not in list):
-    print("b does not exist in the list")
-else:
-    print("b exist in the list")
 
 
 """
