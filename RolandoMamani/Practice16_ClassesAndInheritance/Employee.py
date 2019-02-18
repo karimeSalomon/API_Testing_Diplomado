821ee1f5ef13ca822b01a39b25864360c7f1166c
HrodLandM <mclrolando@gmail.com>
2019-02-17 20:59:55 -0400

    updating

--- RolandoMamani/Practice16_ClassesAndInheritance/Employee.py
+++ RolandoMamani/Practice16_ClassesAndInheritance/Employee.py
@@ -1,8 +1,9 @@
-class Employee
+class Employee:
 
     employee_id = None
     employee_department = None
 
-    def __init__(self, employee_id, employee_department):
+    def __init__(self, name, last_name, age, ci, employee_id, employee_department):
         self.employee_id = employee_id
         self.employee_department = employee_department
+        super().__init__(firs_name, last_name, age, ci)
--- RolandoMamani/Practice16_ClassesAndInheritance/Person.py
+++ RolandoMamani/Practice16_ClassesAndInheritance/Person.py
@@ -1,11 +1,14 @@
 class Person:
-    name = None
+    first_name = None
     last_name = None
     age = None
     ci = None
 
     def __init__(self, name, last_name, age, ci):
-        self.name = name
+        self.first_name = name
         self.last_name = last_name
         self.age = age
         self.ci = ci
+
+    def name(self):
+        return self.first_name + " " + self.last_name
