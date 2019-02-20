dc68808fbd1fb4167dfd6676537fbf34521bb426
Mary Luna <jimenaluna84@gmail.com>
2019-02-19 20:13:42 -0400

    Behave

--- MaryLuna/isPrime.py
+++ MaryLuna/isPrime.py
@@ -17,3 +17,4 @@ def isPrime(numMin, numMax):
 
 
 isPrime(1, 10)
+
---
+++ MaryLuna/listTest.py
@@ -0,0 +1,13 @@
+
+texto = "fadsaasdasdasdasdafdfdsfdsf"
+
+d = {}
+for letra in texto:
+    if d.has_key(letra):
+        d[letra] = d[letra] + 1
+    else:
+        d[letra] = 1
+
+print d
+print(d.keys())
+print (d.values())
